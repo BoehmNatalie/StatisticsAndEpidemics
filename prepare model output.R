e485fb7f2271eb9fb4864dabaf52f76eec47db69
BoehmNatalie <natalie@uboehm.de>
2025-03-14 11:18:02 +0100

    Update prepare model output.R

--- prepare model output.R
+++ prepare model output.R
@@ -8,10 +8,12 @@ csv_files <- list.files(pattern = "\\.csv$")
 data_list <- lapply(csv_files, read.csv)
 combined_data <- do.call(rbind, data_list)
 
-write.csv(combined_data,"/Epinow/GE/summaryGE.csv",row.names=FALSE)
-
-########## Kassteele fix quantile names ##########
-df_summary<- read.csv("Kassteele/GE/KasGEfinalsummary2_7.csv")
+write.csv(combined_data,"summaryGE.csv",row.names=FALSE)
+setwd("..")
+setwd("..")
+setwd("..")
+########## Kassteele fix quantile names for nowcasts ##########
+df_summary<- read.csv("Kassteele/GE/KasGEnowcast2_7.csv")
 df <-df_summary
 df <- df %>%
   rename(q5 = lwr)
@@ -22,10 +24,10 @@ df <- df %>%
 df <- df %>% 
   mutate(forecast_date = as.Date(forecast_date)) %>% 
   filter(format(forecast_date, "%m") != "07")
-#write.csv(df,"Epinow/GEfinalsummary15_2.csv",row.names=FALSE)
+write.csv(df,"Kassteele/GEnowcast.csv",row.names=FALSE)
 
-########## Epinowcast fix columnnames ##########
-combined_data <- combined_data %>%
-  rename(forecast_date = report_date)
-combined_data <- combined_data %>%
-  rename(target_end_date = reference_date)
+########## Epinowcast fix columnnames only for nowcasts not finaly summary ##########
+#combined_data <- combined_data %>%
+#  rename(forecast_date = report_date)
+#combined_data <- combined_data %>%
+#  rename(target_end_date = reference_date)
